ee2f5760ce67e4a307d5951d16b9e31563611ed7
Mahdi Mohammadi <mm@binary.com>
2016-05-20 22:58:12 +0200

    calculate classpath dynamically

--- JCompiler.java
+++ JCompiler.java
@@ -51,21 +51,33 @@ public class JCompiler {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            URI requestedUri = t.getRequestURI();
-            String fileToCheck = requestedUri.getRawQuery();
-
+            String response = "";
             long startTime = System.nanoTime();
-            if ( lastCheckedFile == null || !lastCheckedFile.equals(fileToCheck) ) {
-                 lastCheckedFile = fileToCheck;
-                 fileManager.close();
-                 fileManager = compiler.getStandardFileManager(null, null, null);
+
+            try
+            {
+                URI requestedUri = t.getRequestURI();
+                String fileToCheck = requestedUri.getRawQuery();
+
+                if ( lastCheckedFile == null || !lastCheckedFile.equals(fileToCheck) ) {
+                     lastCheckedFile = fileToCheck;
+                     fileManager.close();
+                     fileManager = compiler.getStandardFileManager(null, null, null);
+                }
+
+                response = myObject.compile(requestedUri.getRawQuery());
+            }
+            catch(IOException exc) {
+                response = exc.getMessage();
+            }
+            catch (Exception exc) {
+                response = exc.getMessage();
             }
 
-            String response = myObject.compile(requestedUri.getRawQuery());
             long estimatedTime = System.nanoTime() - startTime;
             estimatedTime /= 1000;
             estimatedTime /= 1000;
-            response += "\n(took "+String.valueOf(estimatedTime) + " ms)";
+            response += "\n(took "+String.valueOf(estimatedTime) + " ms)\n";
             
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
@@ -87,25 +99,72 @@ public class JCompiler {
             if ( new File(parentPath, "pom.xml").exists() ) {
                 return parentPath + "/src/main/java/";
             }
+            if ( new File(parentPath, "build.xml").exists() ) {
+                return parentPath + "/src/java/";
+            }
         }
 
         return null;
     }
 
+    private List<String> getJarFiles(String path) {
+        File dir = new File(path);
+        List<String> result = new ArrayList<String>(); 
+
+
+        for (final File fileEntry : dir.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                List<String> temp = getJarFiles(fileEntry.getAbsolutePath());
+                result.addAll(temp); 
+            } else {
+                result.add(fileEntry.getAbsolutePath());
+            }
+        }
+
+        return result;
+    }
+
+    private String getClassPath(String filePath) {
+        //go up in the file hierarchy until you find a pom.xml or else return null
+        File file = new File(filePath);
+        String classPath = ".";
+
+        while ( file != null ) {
+            String parentPath = file.getAbsoluteFile().getParent();
+
+            if ( parentPath == null || parentPath.equals("/") ) return classPath;
+            file = new File(parentPath);
+
+            if ( new File(parentPath, "/lib").exists() ) {
+                for(String f : getJarFiles(parentPath+"/lib")) {
+                    classPath += ":" + f;
+                }
+            }
+            if ( new File(parentPath, "/build/classes/main").exists() ) {
+                classPath += ":" + parentPath + "/build/classes/main";
+            }
+        }
+
+        return classPath;
+    }
 
     private String compile(String file) throws IOException {
         DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
         Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromStrings(Arrays.asList(file));
 
-        String sourcePath = getSourcePath(file);
-
         List<String> optionList = new ArrayList<String>();
+
+        String sourcePath = getSourcePath(file);
         if ( sourcePath != null ) {
             optionList.addAll(Arrays.asList("-sourcepath",sourcePath));
         }
 
-        // set compiler's classpath to be same as the runtime's
-        optionList.addAll(Arrays.asList("-classpath",System.getenv("CLASSPATH")));
+        String classPath = getClassPath(file);
+        classPath += ":/home/vagrant/cassandra/lib/netty-all-4.0.36.Final.jar";
+        if ( classPath != null ) {
+            optionList.addAll(Arrays.asList("-classpath",classPath));
+        }
+
         optionList.addAll(Arrays.asList("-d","/tmp"));
 
         JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, optionList,
@@ -117,12 +176,24 @@ public class JCompiler {
             StringBuilder sb = new StringBuilder();
             for (Diagnostic<?> diagnostic : diagnostics.getDiagnostics()) {
                 //d.getCode returns compiler.err.class.public.should.be.in.file 
-                sb.append(String.format("At %d:%d ->  %s\n", diagnostic.getLineNumber(), diagnostic.getColumnNumber(), diagnostic.getMessage(null)));
+                sb.append(String.format("[%s] At %d:%d ->  %s\n", shortenKind(diagnostic.getKind().toString()), diagnostic.getLineNumber(), diagnostic.getColumnNumber(), diagnostic.getMessage(null)));
             }
 
             return sb.toString();
         } else {
             return "OK!\n";
         }
+    }
+
+    private String shortenKind(String kind) {
+        switch ( kind ) {
+            case "ERROR": return "ERR";
+            case "MANDATORY_WARNING": return "MWR";
+            case "WARNING": return "WRN";
+            case "NOTE": return "NTE";
+            case "OTHER": return "OTH";
+        }
+
+        return "???";
     }
 }
